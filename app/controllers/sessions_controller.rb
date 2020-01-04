375e7de794f19e9f8d676803e6f93d20f806b07a
github username <github email address>
2020-01-04 14:34:17 -0500

    added a create action that authenticates a user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+  
+  def new
+
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.try(:authenticate, params[:session][:password])
+       session[:user_id] = @user.id
+       render json: @user
+    else
+      render json: {
+        error: "INVALID"
+      }
+    end
+  end
 end
