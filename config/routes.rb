3e52cd7fcf794e555e31c7f09ca03fef103f5bbd
github username <github email address>
2020-01-04 14:36:19 -0500

    renamed :sessions routes for logging in

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :activities
   resources :events
   resources :users
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
