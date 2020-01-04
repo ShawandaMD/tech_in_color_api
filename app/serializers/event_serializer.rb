7f784f918734bf7ab2998782f48d675807cad036
github username <github email address>
2020-01-04 14:59:10 -0500

    added attributes and relationships

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,3 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :title
+  attributes :id, :title, :description, :organizer, :date, :time, :link
+  belongs_to :user
+  has_many :activities
 end
