90d9f859d6f3606310dae6b538620fb73ae03b30
github username <github email address>
2020-01-08 11:46:23 -0500

    removed new action and added pry to create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
 
-  def new
-    # binding.pry
-  end
+  # def new
+  #   binding.pry
+  # end
 
   def create
+    binding.pry
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.try(:authenticate, params[:session][:password])
        session[:user_id] = @user.id
