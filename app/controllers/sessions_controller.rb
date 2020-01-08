10a11476b3a21c9c9e257f86d6d2de8fa71b9751
github username <github email address>
2020-01-08 12:58:01 -0500

    commented out pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   # end
 
   def create
-    binding.pry
+    # binding.pry
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.try(:authenticate, params[:session][:password])
        session[:user_id] = @user.id
