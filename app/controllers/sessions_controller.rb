b3b8ffa884ba3514334cbb03676708f7363c9a66
github username <github email address>
2020-01-10 13:32:17 -0500

    created get_current_user action and added comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,27 @@
 class SessionsController < ApplicationController
 
-  # def new
-  #   binding.pry
-  # end
-
+  # responding to post fetch request
   def create
-    # binding.pry
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.try(:authenticate, params[:session][:password])
        session[:user_id] = @user.id
        render json: @user
     else
       render json: {
-        error: "INVALID"
+        error: "Invalid login credentials"
       }
     end
   end
+
+  #responding to get fetch request because we already have the user.id 
+  def get_current_user
+    if logged_in?
+      render json: current_user
+    else
+      render json: {
+        error: "No user logged in"
+      }
+    end
+  end
+
 end
