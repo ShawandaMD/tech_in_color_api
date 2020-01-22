e2c66d9f2219a70d7f11adbd33404b1651391d94
github username <github email address>
2020-01-22 13:29:44 -0500

    added presences of valdations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :activities
-  has_many :events, through: :activities  
+  has_many :events, through: :activities
+  validates :email, :f_name, :l_name, :city, :state, :zip_code, presence: true
 end
