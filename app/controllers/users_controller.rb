419113d6534c1306a2ba6bfa8d5c85351df95214
github username <github email address>
2020-01-23 12:27:03 -0500

    in create action when a user is being saved it will set session id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,14 +18,15 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-    binding.pry
+
     if @user.save
+      session[:user_id] = @user.id
       render :show, status: :created, location: @user
     else
       resp = {
         error: @user.errors.full_messages.to_sentence
       }
-      render json: @user.errors, status: :unprocessable_entity
+      render json: resp, status: :unprocessable_entity
     end
   end
 
