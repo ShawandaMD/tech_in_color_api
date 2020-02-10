1e64a650a111914cb3cf19d541a12434f1470f15
ShawandaMD <shawandadavis93@gmail.com>
2020-02-10 16:46:07 -0500

    changed validations removed zipped code

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   has_many :activities
   has_many :events, through: :activities
-  validates :email, :f_name, :l_name, :city, :state, :zip_code, presence: true
+  validates :email, :f_name, :l_name, :city, :state, presence: true
 end
