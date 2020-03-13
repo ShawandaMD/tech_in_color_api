c16fb73e45e6013bfdf6cb8e39febe66b3e07b9b
ShawandaMD <shawandadavis93@gmail.com>
2020-03-13 13:45:54 -0400

    reset entire database

--- db/migrate/20200104184226_add_columns_to_activity.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToActivity < ActiveRecord::Migration[5.2]
-  def change
-    add_column :activities, :meetup_id, :integer
-    add_column :activities, :meetup_link, :string
-  end
-end
