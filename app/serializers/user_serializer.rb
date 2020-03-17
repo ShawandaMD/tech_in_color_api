1b5bec3c3ed51c72374c91eefd3249227953a29f
ShawandaMD <shawandadavis93@gmail.com>
2020-03-17 14:54:02 -0400

    changed the relationships

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :f_name, :l_name, :email, :city, :state, :zip_code
-  has_many :events 
+  attributes :id, :full_name, :email, :city, :state
+  has_many :events
 end
